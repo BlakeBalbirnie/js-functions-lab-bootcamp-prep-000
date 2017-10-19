c16c31c616dfb26fff86e8c11346c8bcf7f1af74
Blake Balbirnie <blakebalbirnie@gmail.com>
2017-10-19 19:22:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ function happyHolidays(string) {
 }
 
 function happyHolidaysTo(name) {
-  return "Happy holidays, ${name}!"
+  return 'Happy holidays, ${name}!'
 }
 
 function happyHolidaysTo(holiday, name) {
-  return "Happy holidays, ${holiday}, ${name}!"
+  return 'Happy holidays, ${holiday}, ${name}!'
 }
